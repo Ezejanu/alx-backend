64dd3ac9d9d68607a548a7afe23838ede57bbc1f
Ezejanu <Ezejanu_nno@ymail.com>
2024-03-12 18:13:07 +0000

    pycodestyle error fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,9 +6,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', page_title='Welcome to Holberton',
+                           header='Hello world')
 
 
 if __name__ == '__main__':
